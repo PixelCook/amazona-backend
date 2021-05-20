8e6d2cd0c7bab743543aad208f8231f8ea256931
PixelCook <zacharyigould@gmail.com>
2021-05-20 20:54:03 +0300

    update

---
+++ data.js
@@ -0,0 +1,32 @@
+export default {
+    products: [ {
+        _id: '1',
+        name: 'Slim Shirt',
+        brand: 'Nike',
+        price: 60,
+        image: '/images/d1.jpeg',
+        category: 'shirts',
+        rating: 4.5,
+        reviewNum: 10
+},
+{
+    _id: '2',
+    name: 'Fit Shirt',
+    brand: 'Addidas',
+    price: 70,
+    image: '/images/d1.jpeg',
+    category: 'shirts',
+    rating: 5,
+    reviewNum: 10
+},
+{
+    _id: '3',
+    name: 'Best Shirt',
+    brand: 'Nike',
+    price: 60,
+    category: 'shirts',
+    image: '/images/d1.jpeg',
+    rating: 4.5,
+    reviewNum: 10
+}],
+}
